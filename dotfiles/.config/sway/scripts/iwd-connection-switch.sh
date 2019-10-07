538ff3253aea0f21cef4bfdf36a0db9f5bab5396
Ivan <yumteej@gmail.com>
2019-10-07 21:19:16 +0300

    iwd wifi/wired connection switching script

---
+++ dotfiles/.config/sway/scripts/iwd-connection-switch.sh
@@ -0,0 +1,8 @@
+wiredup=$(ip link | grep 'enp4s0' | grep -o 'state UP')
+wireddown=$(ip link | grep 'enp4s0' | grep -o 'state DOWN')
+
+if [ "$wiredup" = "state UP" ]; then
+	iwctl -- station wlp2s0 disconnect
+elif [ "$wireddown" = "state DOWN" ]; then
+	iwctl -- station wlp2s0 connect speed_conditioned
+fi
