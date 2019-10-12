9915b89cffad5971f7c764803d02eb0c3b77c783
Ivan <yumteej@gmail.com>
2019-10-12 18:25:44 +0300

    sway scripts update

--- dotfiles/.config/sway/scripts/iwd-connection-switch.sh
+++ dotfiles/.config/sway/scripts/iwd-connection-switch.sh
@@ -1,8 +1,10 @@
+#!/bin/sh
+
 wiredup=$(ip link | grep 'enp4s0' | grep -o 'state UP')
 wireddown=$(ip link | grep 'enp4s0' | grep -o 'state DOWN')
 
 if [ "$wiredup" = "state UP" ]; then
-	iwctl -- station wlp2s0 disconnect
+    iwctl -- station wlp2s0 disconnect
 elif [ "$wireddown" = "state DOWN" ]; then
-	iwctl -- station wlp2s0 connect speed_conditioned
+    iwctl -- station wlp2s0 connect speed_conditioned
 fi
--- dotfiles/.config/sway/scripts/kbd_layout.sh
+++ dotfiles/.config/sway/scripts/kbd_layout.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/bin/sh
+
 kbdlayout=$(swaymsg -r -t get_inputs | awk '/1:1:AT_Translated_Set_2_keyboard/;/xkb_active_layout_name/' | grep -A1 '\b1:1:AT_Translated_Set_2_keyboard\b' | grep "xkb_active_layout_name" | awk -F '"' '{print $4}')
 
 if [ "$kbdlayout" = "English (US)" ]; then
-	echo US
+    echo US
 elif [ "$kbdlayout" = "Russian" ]; then
-	echo RU
+    echo RU
 else
-	echo UA
+    echo UA
 fi
