6cb81576ca21b52a19fe9d68737686e8350f492b
Ivan <yumteej@gmail.com>
2019-10-19 18:47:56 +0300

    disable net interface renaming

--- dotfiles/user/.config/sway/scripts/iwd-connection-switch.sh
+++ dotfiles/user/.config/sway/scripts/iwd-connection-switch.sh
@@ -1,7 +1,5 @@
-#!/bin/sh
-
-wiredup=$(ip link | grep 'enp4s0' | grep -o 'state UP')
-wireddown=$(ip link | grep 'enp4s0' | grep -o 'state DOWN')
+wiredup=$(ip link | grep 'eth0' | grep -o 'state UP')
+wireddown=$(ip link | grep 'eth0' | grep -o 'state DOWN')
 
 if [ "$wiredup" = "state UP" ]; then
     iwctl -- station wlan0 disconnect
