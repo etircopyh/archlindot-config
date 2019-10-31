ec9b45a21732ce89e437ac17635c1a4cf827edef
Ivan <etircopyhdot@gmail.com>
2019-10-31 06:02:01 +0200

    iwd connection script autostart

--- dotfiles/user/.config/autostart-scripts/iwd-connection-switch.sh
+++ dotfiles/user/.config/autostart-scripts/iwd-connection-switch.sh
@@ -3,7 +3,7 @@ wiredup=$(ip link | grep 'eth0' | grep -o 'state UP')
 wireddown=$(ip link | grep 'eth0' | grep -o 'state DOWN')
 
 if [ "$wiredup" = "state UP" ]; then
-    iwctl -- station wlan0 disconnect
+    iwctl station wlan0 disconnect
 elif [ "$wireddown" = "state DOWN" ]; then
-    iwctl -- station wlan0 connect speed_conditioned
+    iwctl station wlan0 connect speed_conditioned
 fi
