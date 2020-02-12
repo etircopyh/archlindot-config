f6129bbe7375598e98e714b969b1204a61c860e3
Ivan <etircopyhdot@gmail.com>
2020-02-12 19:27:29 +0200

    sway scripts

---
+++ dotfiles/user/.config/sway/scripts/screencast.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+filename=$(date +'%Y-%m-%d-%H:%M_screencast')
+isscreencastup=$(pgrep -a wf-recorder)
+
+screencast=$(wf-recorder -a -f ~/Screencasts/$filename.mp4)
+
+if [[ ! -z "$isscreencastup" ]]; then
+    exec $screencast 
+else
+    pkill --signal INT wf-recorder
+fi
---
+++ dotfiles/user/.config/sway/scripts/screenshot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+FILE=~/Screenshots/$(swaymsg -t subscribe -m '["window"]' | jq -r '.container.name'date +'%Y-%m-%d-%H:%M:%S_screenshot').png
+
+grim $FILE
+
+wl-copy -t image/png < $FILE
