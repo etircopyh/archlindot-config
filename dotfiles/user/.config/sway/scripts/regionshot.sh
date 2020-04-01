1de2b1b58c88658f352b40a3161499481fb1da3a
Ivan <etircopyhdot@gmail.com>
2020-04-01 03:41:50 +0300

    fixed white long hole up there

---
+++ dotfiles/user/.config/sway/scripts/regionshot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+FILE=~/Screenshots/$(date +'%Y-%m-%d-%H:%M:%S_regionshot').png
+
+grim -g "$(slurp -c '#990000FF' -s '#2F4F4F28' -b '#00000046' -d)" $FILE
+
+wl-copy -t image/png < $FILE
---
+++ dotfiles/user/.config/sway/scripts/windowshot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+FILE=~/Screenshots/$(date +'%Y-%m-%d-%H:%M:%S_windowshot').png
+
+grim -g "$(swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible? and .focused == true) | .rect | "\(.x),\(.y) \(.width)x\(.height)"')" $FILE
+
+wl-copy -t image/png < $FILE
