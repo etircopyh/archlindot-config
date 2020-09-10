6e0230433eb3c9ef3145913e9e6b0245c3559588
Ivan <etircopyhdot@gmail.com>
2020-09-11 01:07:25 +0300

    vim-plug update shell script

--- dotfiles/user/.scripts/vim-plug-update.sh
+++ dotfiles/user/.scripts/vim-plug-update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ] || [ "$(/usr/bin/diff ~/.local/share/nvim/site/autoload/plug.vim <(curl -fsSL https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim))" ]; then
+if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ] || [ "$(/usr/bin/diff ~/.local/share/nvim/site/autoload/plug.vim <(curl -fsSL https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim) >&1)" ]; then
     echo 'Updating vim-plug...'
     exec curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 elif [ ! -d ~/.local/share/nvim/plugged/ ] || [ ! "$(/usr/bin/ls -A ~/.local/share/nvim/plugged/)" ]; then
