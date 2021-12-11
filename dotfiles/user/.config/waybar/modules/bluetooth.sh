c070fdbe91637a21abbfa7867852746a06e96f43
Ivan <etircopyhdot@gmail.com>
2021-12-12 01:09:54 +0200

    scripts: Update waybar bluetooth module

--- dotfiles/user/.config/waybar/modules/bluetooth.sh
+++ dotfiles/user/.config/waybar/modules/bluetooth.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
-connected=$(bluetoothctl info | rg -e 'Connected: yes')
-disconnected=$(bluetoothctl info | rg -e 'Missing device')
-btname=$(bluetoothctl info | rg Name: | awk '{$1=""; print $0}')
-phone=$(bluetoothctl info | rg -e 'Icon: phone')
-computer=$(bluetoothctl info | rg -e 'Icon: computer')
-headset=$(bluetoothctl info | rg -e 'Icon: audio' -e 'Headset  ')
+
+set -o nounset
+set -o pipefail
+
+
+connected=$(bluetoothctl info | grep -e 'Connected: yes')
+disconnected=$(bluetoothctl info | grep -e 'Missing device')
+phone=$(bluetoothctl info | grep -e 'Icon: phone')
+computer=$(bluetoothctl info | grep -e 'Icon: computer')
+headset=$(bluetoothctl info | grep -e 'Icon: audio-headset')
+
+devicemac=$(bluetoothctl info | head -1 | awk '{print $2}' | sed 's/:/_/g')
+
+[ "$connected" ] && batterylevel=$(dbus-send --print-reply=literal --system --dest=org.bluez "/org/bluez/hci0/dev_$devicemac" org.freedesktop.DBus.Properties.Get string:"org.bluez.Battery1" string:"Percentage" | awk '{print $3}' | awk '{print $0"%"}')
+btname=$(bluetoothctl info | grep 'Name: ' | awk '{$1=""; sub(/^[ ]+/, ""); print $0}')
 
 if [ "$phone" ] && [ "$connected" ]; then
     echo "{\"text\": \"$btname\", \"class\": \"phone\", \"alt\": \"phone\"}"
 elif [ "$computer" ] && [ "$connected" ]; then
     echo "{\"text\": \"$btname\", \"class\": \"computer\", \"alt\": \"computer\"}"
 elif [ "$headset" ] && [ "$connected" ]; then
-    echo "{\"text\": \"$btname\", \"class\": \"headset\", \"alt\": \"headset\"}"
+    echo "{\"text\": \"$btname ${batterylevel}\", \"class\": \"headset\", \"alt\": \"headset\"}"
 elif [ "$disconnected" ]; then
     echo "{\"text\": \"OFF\", \"class\": \"bt-off\", \"alt\": \"disconnected\"}"
 fi
