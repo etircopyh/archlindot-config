dcd5e336fc954f4629f25ed4553a52c03c4034ef
Ivan <etircopyhdot@gmail.com>
2021-12-30 03:04:36 +0200

    waybar: Update bluetooth module

--- dotfiles/user/.config/waybar/modules/bluetooth.sh
+++ dotfiles/user/.config/waybar/modules/bluetooth.sh
@@ -3,24 +3,22 @@
 set -o nounset
 set -o pipefail
 
-
-connected=$(bluetoothctl info | grep -e 'Connected: yes')
-disconnected=$(bluetoothctl info | grep -e 'Missing device')
-phone=$(bluetoothctl info | grep -e 'Icon: phone')
-computer=$(bluetoothctl info | grep -e 'Icon: computer')
-headset=$(bluetoothctl info | grep -e 'Icon: audio-headset')
-
 devicemac=$(bluetoothctl info | head -1 | awk '{print $2}' | sed 's/:/_/g')
 
-[ "$connected" ] && batterylevel=$(dbus-send --print-reply=literal --system --dest=org.bluez "/org/bluez/hci0/dev_$devicemac" org.freedesktop.DBus.Properties.Get string:"org.bluez.Battery1" string:"Percentage" | awk '{print $3}' | awk '{print $0"%"}')
-btname=$(bluetoothctl info | grep 'Name: ' | awk '{$1=""; sub(/^[ ]+/, ""); print $0}')
+connected=$(dbus-send --print-reply=literal --system --dest=org.bluez "/org/bluez/hci0/dev_$devicemac" org.freedesktop.DBus.Properties.Get string:"org.bluez.Device1" string:"Connected" | grep 'true')
+disconnected=$(bluetoothctl info | grep -e 'Missing device')
+devicetype=$(dbus-send --print-reply=literal --system --dest=org.bluez "/org/bluez/hci0/dev_$devicemac" org.freedesktop.DBus.Properties.Get string:"org.bluez.Device1" string:"Icon" | awk '{print $2}')
 
-if [ "$phone" ] && [ "$connected" ]; then
+batterylevel=$(dbus-send --print-reply=literal --system --dest=org.bluez "/org/bluez/hci0/dev_$devicemac" org.freedesktop.DBus.Properties.Get string:"org.bluez.Battery1" string:"Percentage" | awk '{print $3}' | awk '{print $0"%"}')
+btname=$(dbus-send --print-reply=literal --system --dest=org.bluez "/org/bluez/hci0/dev_$devicemac" org.freedesktop.DBus.Properties.Get string:"org.bluez.Device1" string:"Name" | awk '{$1=""; sub(/^[ ]+/, ""); print $0}')
+codec=$(pw-dump -N | jq -r '.[].info.props["api.bluez5.codec"] | select( . != null)' | tr '[:lower:]' '[:upper:]')
+
+if [ "$devicetype" = "phone" ] && [ "$connected" ]; then
     echo "{\"text\": \"$btname\", \"class\": \"phone\", \"alt\": \"phone\"}"
-elif [ "$computer" ] && [ "$connected" ]; then
+elif [ "$devicetype" = "computer" ] && [ "$connected" ]; then
     echo "{\"text\": \"$btname\", \"class\": \"computer\", \"alt\": \"computer\"}"
-elif [ "$headset" ] && [ "$connected" ]; then
-    echo "{\"text\": \"$btname ${batterylevel}\", \"class\": \"headset\", \"alt\": \"headset\"}"
+elif [ "$devicetype" = "audio-headset" ] && [ "$connected" ]; then
+    echo "{\"text\": \"$btname [$codec] ${batterylevel}\", \"class\": \"headset\", \"alt\": \"headset\"}"
 elif [ "$disconnected" ]; then
     echo "{\"text\": \"OFF\", \"class\": \"bt-off\", \"alt\": \"disconnected\"}"
 fi
