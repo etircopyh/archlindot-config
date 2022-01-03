dce1c65bd166a8d03fabb4dbf54a67a15c6abbfa
Ivan <etircopyhdot@gmail.com>
2022-01-03 16:25:54 +0200

    waybar: Update bluetooth prompt module

--- dotfiles/user/.config/waybar/modules/bluetooth-prompt.sh
+++ dotfiles/user/.config/waybar/modules/bluetooth-prompt.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
-DEVICE=$(bluetoothctl devices | fzf --layout=reverse | awk '{print $2}')
 
-bluetoothctl connect $DEVICE
+set -o errexit
+set -o nounset
+set -o pipefail
+
+
+if [ -x "$(command -v sk)" ]; then
+    DEVICEPICKER=sk
+    SKIM_DEFAULT_OPTIONS='--ansi --reverse --color=dark --height=96%'
+else
+    DEVICEPICKER=fzf
+    FZF_DEFAULT_OPTS='--ansi --reverse --color=dark --height=96%'
+fi
+
+
+DEVICE=$(bluetoothctl devices | $DEVICEPICKER | awk '{print $2}')
+
+bluetoothctl connect "$DEVICE"
