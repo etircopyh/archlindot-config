f6a68a07f931fd31acccc6ba8e303ae97fecfc51
Ivan <etircopyhdot@gmail.com>
2025-01-15 04:49:35 +0200

    user: add new scripts

--- dotfiles/user/.local/bin/vim-plug-update.sh
+++ dotfiles/user/.local/bin/vim-plug-update.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ] || [ "$(command diff ~/.local/share/nvim/site/autoload/plug.vim <(curl -fsSL https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim) >&1)" ]; then
-    echo 'Updating vim-plug...'
-    exec curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-elif [ ! -d ~/.local/share/nvim/plugged/ ] || [ ! "$(command ls -A ~/.local/share/nvim/plugged/)" ]; then
+VIM_PLUG="${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim"
+PLUGINS_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/nvim/plugged"
+if [ ! -d "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload" ]; then
+    command mkdir -p "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload"
+    echo "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload directory has been created"
+fi
+
+if [ ! -f "$VIM_PLUG" ] || [ "$(command diff "$VIM_PLUG" <(curl -fsSL https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim))" ]; then
+    echo 'Installing/Updating vim-plug...'
+    exec curl -fLo "$VIM_PLUG" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+elif [ ! -d "$PLUGINS_DIR" ] || [ ! "$(command ls -A "$PLUGINS_DIR")" ]; then
     echo 'Installing plugins...'
     exec nvim -es -u init.vim -i NONE -c "PlugInstall" -c "qa"
 else
-    echo 'vim-plug is up to date'
+    echo 'vim-plug is up-to-date'
 fi
